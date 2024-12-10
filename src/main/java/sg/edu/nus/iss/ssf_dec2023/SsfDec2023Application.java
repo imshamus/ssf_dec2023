7c7c1b25359c5368512f33022a434ddea0eb8e67
imshamus <shamus-sjh@hotmail.com>
2024-12-10 22:29:44 +0800

    test commit

---
+++ src/main/java/sg/edu/nus/iss/ssf_dec2023/SsfDec2023Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.ssf_dec2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfDec2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfDec2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/ssf_dec2023/SsfDec2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.ssf_dec2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfDec2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
